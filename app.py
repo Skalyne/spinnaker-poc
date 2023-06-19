bc208236ed9366de0e62eb0c75461c68b096c23f
skalyne <theo.kratz.t@gmail.com>
2023-06-19 06:32:42 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    image_url = 'pikachu.png'  # Replace with the actual path to your image
+    description = 'This is the description of the image.'  # Replace with your desired description
+    return render_template('index.html', image_url=image_url, description=description)
+
+if __name__ == "__main__":
+    app.run()
